627b124c13e41363831717e0251d9f023e5b22c5
Pedro Hammes <hammespedro4@gmail.com>
2022-07-28 08:50:15 -0300

    criei a função que reconhece o operador usado pelo usuário

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
 function test(number){
     let n1 = `${Number(number.value)}`
-    alert(`O número selecionado foi ${n1}`)
+    let resultArea = document.getElementById("result-area")
+    resultArea.innerText += `${n1}`
+}
+
+function operator(symbol){
+    let operator = `${symbol.value}`
+    let resultArea = document.getElementById("result-area")
+    resultArea.innerText += `${operator}`
 }
