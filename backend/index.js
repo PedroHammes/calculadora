685dc44d27f30bb8658f37234c9317ec1880d310
Pedro Hammes <hammespedro4@gmail.com>
2022-08-08 20:25:07 -0300

    resolvi o bug do querySelectorAll

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll(".grid-calculator button")
+console.log(buttons)
+
+// const buttons = document.querySelectorAll("#buttons-container button");
+
 let input = []
 
 function output(button){
