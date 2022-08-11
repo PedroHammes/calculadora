bd6dddda0b9ec1ca72bd20de8dfb9caf1b55a4b8
Pedro Hammes <hammespedro4@gmail.com>
2022-08-11 10:38:21 -0300

    otimização de backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 const buttons = document.querySelectorAll(".grid-calculator button")
-console.log(buttons)
 
-let input = []
-
-function output(button){
-    input += (button.innerText)
-    let resultField = document.getElementById("result-field")
-    resultField.innerText = input
-    return input
+class Equation{
+    constructor(number1, operation, number2){
+        this.number1 = number1
+        this.operation = operation
+        this.number2 = number2
+    } 
 }
 
-function clearAll(){
-    let resultField = document.getElementById("result-field")
-    resultField.innerText = ""
-    input = ""
-}
+
+buttons.forEach((clickedButton) => {
+    if(Number(clickedButton.innerText) >= 0 || clickedButton.innerText == "."){
+        console.log(`Number: ${clickedButton.innerText}`)
+    }else{
+        console.log(`Op: ${clickedButton.innerText}`)
+    }
+})
+
+buttons.forEach((clickedButton) => {
+    clickedButton.addEventListener("click", function(){
+        console.log(clickedButton.innerText)
+    })
+})
+//Usando o forEach para percorrer o array buttons.
+//Para cada elemento do array (clickedButton) eu chamo a função (arrow function =>)
+//A função consiste em adicionar o evento de clique ao botão
+//Quando o botão é clicado ele é exibido no console.log 
