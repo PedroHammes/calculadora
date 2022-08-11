95f88765c7f86f8631f8ca83b8c6ca6198b0c8aa
Pedro Hammes <hammespedro4@gmail.com>
2022-08-11 09:41:14 -0300

    correção de bug

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const buttons = document.querySelectorAll(".grid-calculator button")
 console.log(buttons)
 
-// const buttons = document.querySelectorAll("#buttons-container button");
-
 let input = []
 
 function output(button){
